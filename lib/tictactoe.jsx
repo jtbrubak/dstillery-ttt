fe02ec52dfdea2cd9ae6d24d585cf9cc4362bbda
Jeff Brubaker <jtbrubak1@gmail.com>
2017-11-17 00:57:18 -0500

    Add click logic

--- lib/tictactoe.jsx
+++ lib/tictactoe.jsx
@@ -7,16 +7,27 @@ class Board extends React.Component {
     super();
     this.state = { currentTurn: "X", turnNumber: 1,
       rows: [["", "", ""], ["", "", ""], ["", "", ""]] };
+    this.onClick = this.onClick.bind(this);
+  }
 
+  onClick(row_num, space_num) {
+    let newRows = this.state.rows;
+    newRows[row_num][space_num] = "x";
+    this.setState({ rows: newRows });
+  }
+
+  checkStatus(row_num, space_num) {
+    
   }
 
   render() {
     return (
       <main>
-        {this.state.rows.map((row) =>
+        {this.state.rows.map((row, row_num) =>
           <ul>
-            {row.map((space) =>
-              <li>{space}</li>
+            {row.map((space, space_num) =>
+              <li key={space_num}
+                onClick={() => this.onClick(row_num, space_num)}>{space}</li>
             )}
           </ul>
         )}
