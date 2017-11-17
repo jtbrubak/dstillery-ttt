a9911073fbf2769727e18cb693fdd4e556ced97d
Jeff Brubaker <jtbrubak1@gmail.com>
2017-11-16 23:24:41 -0500

    Add board class

--- board.jsx
+++ board.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 class Board extends React.Component {
+  constructor() {
+    this.state = { currentTurn: "X", turnNumber: 1,
+      rows: [["", "", ""], ["", "", ""], ["", "", ""]] };
 
+  }
+
+  render() {
+    return (
+      <main>
+        {this.state.rows.map((row) =>
+          <ul>
+            {row.map((space) =>
+              <li>{space}</li>
+            )}
+          </ul>
+        )}
+      </main>
+    );
+  }
 }
 
 module.exports = Board;
